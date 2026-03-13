72d3a27078c7083034e9c88d6034ff3c4d4d11f0
Thushar776 <thusharr77@gmail.com>
2026-03-13 14:38:21 +0530

    UC1

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,25 @@
+/**
+ * =============================================================
+ * MAIN CLASS – UseCase1HotelBookingApp
+ * =============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Tushar
+ * @version 1.0
+ */
+
 public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
 }
